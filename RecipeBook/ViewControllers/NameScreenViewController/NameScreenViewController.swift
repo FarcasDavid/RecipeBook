599aa89e454b2517b82b362ab0a4e31833ab56ff
FarcasDavid <davidfarcas@Davids-MacBook-Air.local>
2023-08-09 18:03:39 +0300

    RBI-8: - Added outlets for label and text field on name request screen

---
+++ RecipeBook/ViewControllers/NameScreenViewController/NameScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NameScreenViewController.swift
+//  RecipeBook
+//
+//  Created by David Farcas on 09.08.2023.
+//
+
+import UIKit
+
+class NameScreenViewController: UIViewController {
+
+    // IBOutlets
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var nameTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
