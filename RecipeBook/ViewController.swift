6a8d608045194e883153affab28336a07d95a156
FarcasDavid <davidfarcas@Davids-MacBook-Air.local>
2023-08-09 14:31:05 +0300

    Initial commit

---
+++ RecipeBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeBook
+//
+//  Created by David Farcas on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    var david = 1
+
+
+}
